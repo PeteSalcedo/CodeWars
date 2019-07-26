f9d844aefcb94b27589b9d41ce97042be1922b84
PeteSalcedo <peter.salcedo.diaz@gmail.com>
2019-07-25 20:15:01 -0500

    Adding the solution to the magic calculator

--- MagicCalculator.js
+++ MagicCalculator.js
@@ -32,14 +32,43 @@ An Operation can only call a Value
 A Value should be resolvable to a primitive integer, if needed as such
 
 
-
-var Magic = function (){
-    var value= 0;
+var Magic = function() {
+    var value = 0;
+    
     var operators = {
-        'plus': (a, b) => a + b,
-        'minus': (a, b) => a - b,
-        'times': (a, b) => a * b,
-        'dividedBy': (a, b) => a / b
-    }
+      'plus': (a, b) => a + b,
+      'minus': (a, b) => a - b,
+      'times': (a, b) => a * b,
+      'dividedBy': (a, b) => a / b
     };
-    var FluentCalculator = Magic;
+    
+    var numbers = [
+      'zero', 'one', 'two', 'three', 'four', 'five',
+      'six', 'seven', 'eight', 'nine', 'ten'
+    ];
+    
+    Object.keys(operators).forEach((operator) => {
+      var operatorFunction = operators[operator];
+      var operatorObject = {};
+      
+      numbers.forEach((num, index) => {
+        Object.defineProperty(operatorObject, num, {
+          get: () => value = operatorFunction(value, index)
+        });
+      });
+      
+      Number.prototype[operator] = operatorObject;
+    });
+    
+    numbers.forEach((num, index) => {
+      Object.defineProperty(this, num, {
+        get: () => {
+          value = index;
+          return Number(index);
+        }
+      });
+    });
+  }
+  
+  var FluentCalculator = new Magic();
+  console.log(FluentCalculator.two.plus.three); // 5
